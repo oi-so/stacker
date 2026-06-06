6f9a42569650136f7f7666f18e1d132f55cbd8c3
oi-so <mail@oioi-2020.com>
2026-06-07 01:49:16 +0900

    ioの__init__.pyを更新

--- src/astro_stacker/io/__init__.py
+++ src/astro_stacker/io/__init__.py
@@ -2,4 +2,12 @@
 I/O module for Astro Stacker
 """
 
-__all__ = []
+from .image_manager import ImageManager
+from .image_data import AstroImage, AstroImageInfo
+from .loader import load_image
+__all__ = [
+    "ImageManager",
+    "AstroImage",
+    "AstroImageInfo",
+    "load_image",
+]
