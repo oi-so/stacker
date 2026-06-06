1f7debf7b9dc625e52687c04dd4c6538a82ca8d3
oi-so <mail@oioi-2020.com>
2026-06-06 20:56:26 +0900

    image_managerクラスを追加

---
+++ src/astro_stacker/io/image_manager.py
@@ -0,0 +1,58 @@
+from collections import OrderedDict
+import numpy as np
+
+from .image_data import AstroImage
+from .loader import load_image
+
+
+class ImageManager:
+    def __init__(self, max_loaded_image_count: int = 5) -> None:
+        self.__loaded_images: OrderedDict[int, AstroImage] = OrderedDict()
+        self.loaded_image_size = 0
+        self.max_loaded_image_count = max_loaded_image_count
+
+    def __evict_if_needed(self) -> None:
+        while len(self.__loaded_images) > self.max_loaded_image_count:
+            _, image = self.__loaded_images.popitem(last=False)
+            image.data = None
+    
+
+    def get_data(self, image: AstroImage) -> np.ndarray:
+        if image.data is not None:
+            image.data = load_image(image)
+
+        key = id(image)
+        if key in self.__loaded_images:
+            self.__loaded_images.move_to_end(key)
+        else:
+            self.__loaded_images[key] = image
+
+        self.__evict_if_needed()
+        return image.data
+
+
+    def load(self, image: AstroImage) -> None:
+        self.get_data(image)
+
+
+    def unload(self, image: AstroImage) -> None:
+        key = id(image)
+        if key in self.__loaded_images:
+            del self.__loaded_images[key]
+
+        image.data = None
+
+
+    def unload_all(self) -> None:
+        for image in self.__loaded_images.values():
+            image.data = None
+        self.__loaded_images.clear()
+
+
+    def is_loaded(self, image: AstroImage) -> bool:
+        return id(image) in self.__loaded_images
+    
+
+    def loaded_count(self) -> int:
+        return len(self.__loaded_images)
+    
