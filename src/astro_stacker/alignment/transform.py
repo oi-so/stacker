9f9190bfbc27b80b4c40502442ad16da2e456945
oi-so <mail@oioi-2020.com>
2026-06-08 22:52:25 +0900

    アライメント後画像を取得するapply.pyの名前がわかりにくかったのでtransform.pyに変更し、Provider化

---
+++ src/astro_stacker/alignment/transform.py
@@ -0,0 +1,51 @@
+"""Apply alignment transformations to images."""
+
+from skimage.transform import SimilarityTransform, warp
+import numpy as np
+
+from ..io.image_data import TransformData, AstroImage
+from ..core.provider import FrameProvider
+
+
+
+class ImageTransformer:
+    def apply_transform(
+        self,
+        image: np.ndarray,
+        transform: TransformData
+    ) -> np.ndarray:
+        """Apply pre-computed transformation to an image.
+        
+        Args:
+            image: Input image array
+            transform: TransformData containing transformation matrix
+            
+        Returns:
+            Transformed image with same dtype as input
+        """
+
+        t = SimilarityTransform(
+            matrix=transform.matrix
+        )
+
+        return warp(
+            image,
+            inverse_map=t.inverse,
+            preserve_range=True
+        ).astype(np.float32)
+
+
+
+class AlignedFrameProvider:
+    def __init__(self, base_provider: FrameProvider, transformer: ImageTransformer) -> None:
+        self.base_provider = base_provider
+        self.transformer = transformer
+
+    
+    def get_image(self, astro_image: AstroImage) -> np.ndarray:
+        image = self.base_provider.get_image(astro_image)
+
+        if astro_image.info.transform is None:
+            return image
+        
+        return self.transformer.apply_transform(image, astro_image.info.transform)
