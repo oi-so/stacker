967e67e744d911729c0b325d50db79dfa7086f93
oi-so <mail@oioi-2020.com>
2026-06-09 22:42:52 +0900

    スタック用パイプラインからアライメント処理をパイプラインに移し、providerを使用するように変更

--- src/astro_stacker/pipeline/stacking_pipeline.py
+++ src/astro_stacker/pipeline/stacking_pipeline.py
@@ -1,82 +1,23 @@
-from typing import Iterable
-
 import numpy as np
 
 from ..alignment.aligner import align_catalogs
 from ..alignment.transform import AlignedFrameProvider, ImageTransformer
-from ..core.provider import ImageManagerProvider
-from ..io.image_data import AstroImage
-from ..io.image_manager import ImageManager
+from ..core.provider import FrameProvider
 from ..stacking.combiner import ImageCombiner, Method
-from ..stars.detector import detect_stars
+from ..project.project import Project
+from ..project.settings import StackingSettings
 
 
 class StackingPipeline:
+    def __init__(self, provider: FrameProvider):
+        self.provider = provider
 
-    def __init__(
-        self,
-        manager: ImageManager,
-    ):
-        self.manager = manager
+    def run(self, project: Project, settings: StackingSettings) -> np.ndarray:
+        aligned_provider = AlignedFrameProvider(self.provider, ImageTransformer())
 
-    def run(
-        self,
-        images: list[AstroImage],
-        method: Method = "mean",
-    ) -> np.ndarray:
-
-        if len(images) == 0:
-            raise ValueError(
-                "No images provided"
-            )
-
-        provider = ImageManagerProvider(
-            self.manager
-        )
-
-        reference = provider.get_image(
-            images[0]
-        )
-
-        reference_catalog = detect_stars(
-            reference
-        )
-
-        for image in images[1:]:
-
-            target = provider.get_image(
-                image
-            )
-
-            target_catalog = detect_stars(
-                target
-            )
-
-            result = align_catalogs(
-                reference_catalog,
-                target_catalog
-            )
-
-            image.info.transform = (
-                result.transform
-            )
-
-            image.info.alignment_data = (
-                result.info
-            )
-
-        aligned_provider = (
-            AlignedFrameProvider(
-                provider,
-                ImageTransformer()
-            )
-        )
-
-        combiner = ImageCombiner(
-            aligned_provider
-        )
+        combiner = ImageCombiner(aligned_provider)
 
         return combiner.combine(
-            images,
-            method
+            project.light_frames,
+            settings.method
         )
