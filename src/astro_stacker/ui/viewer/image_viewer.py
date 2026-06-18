7f307a532f803451acc2aa7b21b4e17e36b753ca
oi-so <mail@oioi-2020.com>
2026-06-18 22:39:05 +0900

    画像を拡大縮小や、0,1キーでリサイズできるように変更

--- src/astro_stacker/ui/viewer/image_viewer.py
+++ src/astro_stacker/ui/viewer/image_viewer.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QImage, QPixmap, QPainter
+from PySide6.QtGui import QImage, QMouseEvent, QPixmap, QPainter
 from PySide6.QtWidgets import QGraphicsPixmapItem, QGraphicsScene, QGraphicsView
 import numpy as np
 
@@ -14,6 +14,31 @@ class ImageViewer(QGraphicsView):
         self.setRenderHint(QPainter.RenderHint.Antialiasing | QPainter.RenderHint.SmoothPixmapTransform)
         self._pixmap_item = None
 
+         # ドラッグで移動
+        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
+
+        # アンカーをマウス位置へ
+        self.setTransformationAnchor(
+            QGraphicsView.ViewportAnchor.AnchorUnderMouse
+        )
+        self.setResizeAnchor(
+            QGraphicsView.ViewportAnchor.AnchorUnderMouse
+        )
+
+        self._zoom = 0
+
+    
+    def fit_image(self):
+        if self._pixmap_item is None:
+            return
+
+        self.fitInView(
+            self.scene.sceneRect(),
+            Qt.AspectRatioMode.KeepAspectRatio
+        )
+
+        self._zoom = 0
+
     def set_image(self, image: np.ndarray | None) -> None:
         self.scene.clear()
         self._pixmap_item = None
@@ -22,12 +47,32 @@ class ImageViewer(QGraphicsView):
         qimage = self._to_qimage(image)
         self._pixmap_item = self.scene.addPixmap(QPixmap.fromImage(qimage))
         self.scene.setSceneRect(self._pixmap_item.boundingRect())
-        self.fitInView(self.scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
+        self.fit_image()
 
     def resizeEvent(self, event):
         super().resizeEvent(event)
-        if self._pixmap_item is not None:
-            self.fitInView(self.scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
+
+        if self._pixmap_item is None:
+            return
+
+        # 全体表示中だけ再Fit
+        if self._zoom == 0:
+            self.fit_image()
+
+    
+    def mouseDoubleClickEvent(self, event: QMouseEvent) -> None:
+        self.fit_image()
+        super().mouseDoubleClickEvent(event)
+
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_0:
+            self.fit_image()
+        elif event.key() == Qt.Key.Key_1:
+            self.resetTransform()
+            self._zoom = 1
+
+        super().keyPressEvent(event)
 
     def _to_qimage(self, image: np.ndarray) -> QImage:
         arr = np.squeeze(np.asarray(image, dtype=np.float32))
@@ -55,3 +100,30 @@ class ImageViewer(QGraphicsView):
             arr8 = arr8[..., :3]
         h, w, _ = arr8.shape
         return QImage(arr8.data, w, h, 3 * w, QImage.Format.Format_RGB888).copy()
+    
+
+
+    def wheelEvent(self, event):
+        if self._pixmap_item is None:
+            return
+
+        factor = 1.25
+
+        if event.angleDelta().y() > 0:
+            scale = factor
+            self._zoom += 1
+        else:
+            scale = 1 / factor
+            self._zoom -= 1
+
+        # 縮小しすぎ防止
+        if self._zoom < -5:
+            self._zoom = -5
+            return
+
+        # 拡大しすぎ防止
+        if self._zoom > 30:
+            self._zoom = 30
+            return
+
+        self.scale(scale, scale)
