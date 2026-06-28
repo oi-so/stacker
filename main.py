c46f89335634c9d9b054771c873e2d7946814ebf
oi-so <mail@oioi-2020.com>
2026-06-28 21:35:15 +0900

    main.pyを作成してそこからエントリーできるようにした

---
+++ main.py
@@ -0,0 +1,4 @@
+from astro_stacker.app import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
--- src/astro_stacker/__main__.py
+++ src/astro_stacker/__main__.py
@@ -2,4 +2,4 @@ from .app import main
 
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
