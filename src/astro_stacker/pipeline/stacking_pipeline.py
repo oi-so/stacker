d66c1d1735f578ba63ac693f16fa33d79347dfcf
oi-so <mail@oioi-2020.com>
2026-07-04 15:46:57 +0900

    スタック時に位置合わせ失敗画像やチェックがついていない画像も使用していた問題を修正

--- src/astro_stacker/pipeline/stacking_pipeline.py
+++ src/astro_stacker/pipeline/stacking_pipeline.py
@@ -15,11 +15,17 @@ class StackingPipeline:
         if project.settings.use_alignment:
             provider = AlignedFrameProvider(provider, ImageTransformer())
 
+        frames = [
+            frame for frame in project.light_frames
+            if frame.info.enabled and (not project.settings.use_alignment or frame.info.is_aligned)
+        ]
+
+
         with timer("StackWorkers", True):
             combiner = ImageCombiner(provider)
 
             result = combiner.combine(
-                project.light_frames,
+                frames,
                 settings.method,
                 progress=progress,
                 is_cancelled=is_cancelled,
